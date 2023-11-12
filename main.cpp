adb51460ae9706817436e58957724062695cf340
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-11-12 17:28:37 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int сircle = 4;
+    int speed = 358;
+    int engine = 254;
+    int wheels = 93;
+    int steeringWheel = 49;
+    int wind = 21;
+    int rain = 17;
+
+    std::cout << "============================" << std::endl;
+    std::cout << "Super race. Circle " << сircle << std::endl;
+    std::cout << "============================" << std::endl;
+    std::cout << "Schumacher (" << speed << ")" << std::endl;
+    std::cout << "============================" << std::endl;
+    std::cout << "Driver: Schumacher" << std::endl;
+    std::cout << "Speed: " << speed << std::endl;
+    std::cout << "-----------------------------" << std::endl;
+    std::cout << "Equipment:" << std::endl;
+    std::cout << "Engine: +" << engine << std::endl;
+    std::cout << "Wheels: +" << wheels << std::endl;
+    std::cout << "Steering wheel: +" << steeringWheel << std::endl;
+    std::cout << "-----------------------------" << std::endl;
+    std::cout << "Bad weather actions:" << std::endl;
+    std::cout << "Wind: -" << wind << std::endl;
+    std::cout << "Rain: -" << rain << std::endl;
+
 }
